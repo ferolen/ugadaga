cadfb5f13c85210c0ff65794ae71b98fd05be455
ferolen <fer01@mail.ru>
2022-10-09 18:29:45 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,10 +1,13 @@
 from random import choice
+from time import time
 
 lw = 5 # word length
 level = -1 # level. The more the rarer world can be guessing
 errColor = 'red' # Error message color
 normalColor = 'red' # normal message color
 vocFileName = 'voc.txt'
+novocFileName = 'novoc.txt'
+score = 0
 
 def getvoc(filename):
   voc = {}
@@ -21,7 +24,11 @@ def guess(voc, ds, lw):
       a.append(i)
   return(choice(a))
 
-def guessed(vc, filename):
+def guessed(vc, novc, filename):
   with open(filename,'w',encoding='utf-8') as fv:
     for i in vc:
       fv.write(f'{i}\t{vc[i]}\n')
+  with open(novocFileName,'a',encoding='utf-8') as fv:
+    for i in novc:
+      fv.write(f'{i}\n')
+    novc = []
